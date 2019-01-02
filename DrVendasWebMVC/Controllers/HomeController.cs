1e7c3f71ce9b1b7b51796b673703bacdfc1bf342
Marco Sena <msena2210@gmail.com>
2019-01-02 14:51:14 -0300

    Primeiro Model-Controller Departamento

---
+++ DrVendasWebMVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using DrVendasWebMVC.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DrVendasWebMVC.Controllers
+{
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult About() {
+            ViewData["Message"] = "Gestão de Vendas.";
+
+            return View();
+        }
+
+        public IActionResult Contact() {
+            ViewData["Message"] = "Seu Contato.";
+
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
